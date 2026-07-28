72e0f991154ee9549ce904eca0351e5815cf181c
deepak-9962 <210823104027@kingsedu.ac.in>
2026-07-28 06:15:06 +0530

    fix: remove google_fonts to fix Vercel web build on Dart 3.12.2 - package was unused (no GoogleFonts.* calls), app already uses bundled Roboto fonts - const FontWeight map in google_fonts crashes dart2js on Dart 3.12.2 regardless of version

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart'; // For kIsWeb
